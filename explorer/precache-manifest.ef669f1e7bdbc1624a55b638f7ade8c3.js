b891eeaf1fdaf67b6eab86458811870c41906344
jdotrjs <jdotrjs@users.noreply.github.com>
2018-10-15 18:42:47 -0700

    update phaser browser

---
+++ explorer/precache-manifest.ef669f1e7bdbc1624a55b638f7ade8c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6df0b87b754a5bcd2f59",
+    "url": "/demos/explorer/static/js/runtime~main.6df0b87b.js"
+  },
+  {
+    "revision": "264a836ca488a8a99ca9",
+    "url": "/demos/explorer/static/js/main.264a836c.chunk.js"
+  },
+  {
+    "revision": "8227df61b1ccd25b74f6",
+    "url": "/demos/explorer/static/js/1.8227df61.chunk.js"
+  },
+  {
+    "revision": "264a836ca488a8a99ca9",
+    "url": "/demos/explorer/static/css/main.b970b09a.chunk.css"
+  },
+  {
+    "revision": "0b284c9ab416aafba1efbe729612abda",
+    "url": "/demos/explorer/index.html"
+  }
+];
